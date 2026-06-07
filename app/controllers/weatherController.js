8fd034c59b385679425739e4029422d0962cd5c7
JustinAndreotti <justinandreotti@gmail.com>
2026-06-07 16:04:10 -0700

    General Cleanup

--- app/controllers/weatherController.js
+++ app/controllers/weatherController.js
@@ -3,8 +3,6 @@ import 'dotenv/config';
 
 const API_URL_CURRENT = `https://api.openweathermap.org/data/2.5/weather?lat=38.43655&lon=-122.65975&appid=${process.env.OPEN_WEATHER_API_KEY}&units=imperial`;
 
-const API_URL_FORECAST = "";
-
 export async function getCurrentWeatherData() {
   const response = await axios.get(API_URL_CURRENT);
   const data = response.data;
